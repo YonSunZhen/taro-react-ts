865fa5f81669d204bc45805971bbcd5fb987964a
YonSunZhen <1315976441@qq.com>
2021-04-23 14:23:29 +0800

    init project

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,25 @@
+import { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+export default class Index extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Hello world555666777888!</Text>
+        
+      </View>
+    )
+  }
+}
---
+++ src/pages/login/login.tsx
@@ -0,0 +1,10 @@
+import { View } from '@tarojs/components';
+import React from 'react';
+
+function Login() {
+  return (
+    <View>Login</View>
+  );
+}
+
+export default Login;
